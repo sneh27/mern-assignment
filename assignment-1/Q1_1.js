91a52ccdcb14b3a0125f1f7d3943c70f8130d870
sneh27 <sagroczz@gmail.com>
2021-04-07 02:33:35 +0530

    Add files via upload

---
+++ assignment-1/Q1_1.js
@@ -0,0 +1,15 @@
+//Assignment - 1 
+//Sneh Aashish Gupta
+//Q1
+console.log("Enter the credits for CFC campus leader");
+var input = 7800;
+input = Number(input);
+if (input>=7500) {
+    console.log("Tera Leader");
+} else if (input >= 6000) {
+    console.log("Gega Leader");
+} else if (input >= 4500) {
+    console.log("Mega Leader");
+} else {
+    console.log("Rising Star");
+}
