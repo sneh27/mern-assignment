3afaac598033355ef94526638c1d0785571baf04
sneh27 <sagroczz@gmail.com>
2021-04-07 02:29:43 +0530

    Delete Q1_2.js

--- Q1_2.js
+++
@@ -1,9 +0,0 @@
-//Assignment - 1 
-//Sneh Aashish Gupta
-//Q2
-
-let amount = prompt("Enter the amount");
-let rate = prompt("Enter the interest rate");
-let time = prompt("Enter the time of loan period");
-let payback = amount * (1+((rate/100)*time));
-console.log("Pay up "+payback+" buddy you took a loan of "+amount+" at simple annual interest rate of "+rate+" for "+time+" years...");
